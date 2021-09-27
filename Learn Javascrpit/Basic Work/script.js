dc791d31fb2b8c7d75666730389e9879e4df87d1
amicableifti <amicableifti@gmail.com>
2021-09-27 07:25:27 +0600

    Add js file primivaraible add

--- Learn Javascrpit/Basic Work/script.js
+++ Learn Javascrpit/Basic Work/script.js
@@ -1,3 +1,12 @@
 console.log('this is my first js code');
 var myName = 'ifti nur';
 console.log(myName);
+/*
+Two Types of datatype;
+1. primitive 2. Non-primitive dtatype */
+
+// #1. Primitive data type
+var firstNumber = 10;
+var sceondNumber = 10.10;
+console.log('firstNumber');
+console.log('sceondNumber');
