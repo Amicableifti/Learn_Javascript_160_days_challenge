d988cef59ef815685dd3c15950f9b02d31f6597b
amicableifti <amicableifti@gmail.com>
2021-09-27 08:51:49 +0600

    Varaible String Add

--- Learn Javascrpit/Basic Work/script.js
+++ Learn Javascrpit/Basic Work/script.js
@@ -10,3 +10,12 @@ var firstNumber = 10;
 var sceondNumber = 10.10;
 console.log('firstNumber');
 console.log('sceondNumber');
+// #2. primitive String
+
+var firstString = ' This is my first string ';
+var sceondString= 'this is my 2ed String l';
+console.log(firstString);
+console.log(sceondString);
+// this is not a string its a numebr
+var isntNumber='this not a number'
+console.log(isntNumber);
